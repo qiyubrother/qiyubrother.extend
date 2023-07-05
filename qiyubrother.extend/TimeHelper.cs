8ad78219d6b3b596935ee9541a25b252ba3ce660
nnd_liuzhenhua <liuzhenhua@nndrobot.com>
2023-07-05 10:27:18 +0800

    增加文件头

--- qiyubrother.extend/TimeHelper.cs
+++ qiyubrother.extend/TimeHelper.cs
@@ -1,4 +1,12 @@
-﻿using System;
+/*
+ * 公司名称：北京麒宇兄弟科技有限公司
+ * 模块功能：
+ * 创建日期：2023-07-05
+ * 修改日期：2023-07-05
+ * 作    者：刘振华2023-07-05
+ * 电子邮箱：13240137763@163.com2023-07-05
+ */
+using System;
 using System.Collections.Generic;
 using System.Text;
 
