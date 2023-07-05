e6fa77e3a44604a9eb700d432e52e021328bf34b
nnd_liuzhenhua <liuzhenhua@nndrobot.com>
2023-07-05 10:46:58 +0800

    0

--- FastColoredTextBox/Tester/Properties/AssemblyInfo.cs
+++ FastColoredTextBox/Tester/Properties/AssemblyInfo.cs
@@ -3,8 +3,8 @@
  * 模块功能：
  * 创建日期：2023-07-05
  * 修改日期：2023-07-05
- * 作    者：刘振华2023-07-05
- * 电子邮箱：13240137763@163.com2023-07-05
+ * 作    者：刘振华
+ * 电子邮箱：13240137763@163.com
  */
 using System.Reflection;
 using System.Runtime.CompilerServices;
